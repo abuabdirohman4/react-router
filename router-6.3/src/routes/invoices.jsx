898890f450c7c03149a0d5ae68e1bfca152fa257
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-10-07 14:05:25 +0700

    6.3 Listing the Invoices

--- router-6.3/src/routes/invoices.jsx
+++ router-6.3/src/routes/invoices.jsx
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom";
+import { getInvoices } from "../data";
+
 export default function Invoices() {
+  let invoices = getInvoices();
   return (
-    <main style={{ padding: "1rem 0" }}>
-      <h2>Invoices</h2>
-    </main>
+    <div style={{ padding: "flex" }}>
+      <nav style={{ borderRight: "solid 1px", padding: "1rem" }}>
+        {invoices.map((invoices) => (
+          <Link
+            style={{ display: "block", margin: "1rem 0" }}
+            key={invoices.number}
+            to={`/invoices/${invoices.number}`}
+          >
+            {invoices.name}
+          </Link>
+        ))}
+      </nav>
+    </div>
   );
 }
