5fed23a3e3ad9a692fec073af7177e5f5941dfe3
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-10-07 14:18:39 +0700

    6.3 Reading URL Params

--- router-6.3/src/main.jsx
+++ router-6.3/src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Expenses from "./routes/expenses";
 import Invoices from "./routes/invoices";
+import Invoice from "./routes/invoice";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -12,7 +13,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route path="expenses" element={<Expenses />} />
-          <Route path="invoices" element={<Invoices />} />
+          <Route path="invoices" element={<Invoices />}>
+            <Route path=":invoiceId" element={<Invoice />} />
+          </Route>
           <Route
             path="*"
             element={
